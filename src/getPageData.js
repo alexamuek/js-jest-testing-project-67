55531d4f555a3b25c061276a932fbde837fcc660
Alex <alexamuek@gmail.com>
2025-04-14 13:54:23 +0800

    add logic to download images

--- src/getPageData.js
+++ src/getPageData.js
@@ -1,21 +1,37 @@
+import { fileURLToPath } from 'url';
 import axios from 'axios';
+import nock from 'nock';
+import fs from 'fs/promises';
+import path, { dirname }  from 'path';
 
-const getData = async (url) => {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const savePath = path.join(__dirname, '..', 'output' , 'downloaded_image.jpg');
+
+const getHTML = async (url) => {
   try {
     const response = await axios.get(url);
     return response.data;
   } catch (error) {
-    if (error.response) {
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      console.log(error.request);
-    } else {
-      console.log('Error', error.message);
-    }
+    console.log(error);
     throw new Error('Something went wrong!');
   }
 };
 
-export default getData;
+const getContent = async (contentUrl) => {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: contentUrl,
+      responseType: 'arraybuffer'
+    });
+    const fileData = Buffer.from(response.data, 'binary');
+    return fileData;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Something went wrong!');
+  }
+};
+
+export { getHTML, getContent };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import path from 'path';
 import _ from 'lodash';
-import getData from './getPageData.js';
+
+import { getHTML } from './getPageData.js';
 import { createFile, isExistedFolder } from './handleFilesTasks.js';
+import { downloadContent } from './downloadContent.js';
 
 const defaultPath = path.join(process.cwd(), 'output');
 
@@ -9,11 +11,14 @@ const loadHTML = async (url, outputPath = defaultPath) => {
   console.log(url);
   console.log(outputPath);
   await isExistedFolder(outputPath);
-  const html = await getData(url);
+  const html = await getHTML(url);
   const URLwithoutProtocol = url.substring(url.indexOf('//') + 2);
   const fileName = `${_.replace(URLwithoutProtocol, /[^0-9a-zA-Z]/g, '-')}.html`;
+  const contentFolder = `${_.replace(URLwithoutProtocol, /[^0-9a-zA-Z]/g, '-')}_files`;
   const fullPath = path.join(outputPath, fileName);
-  await createFile(fullPath, html);
+  
+  const updatedHTML = downloadContent(html, `${outputPath}/${contentFolder}`, url, contentFolder);
+  await createFile(fullPath, html); // заменить на updatedHTML
   return fullPath;
 };
 
