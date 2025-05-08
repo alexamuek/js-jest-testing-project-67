08220c07c929170a79a2fd05017289c75fed67ba
Alex <alexamuek@gmail.com>
2025-05-08 22:27:04 +0800

    default path

--- src/handleFilesTasks.js
+++ src/handleFilesTasks.js
@@ -19,7 +19,7 @@ const createFolderIfNecessary = async (folderPath) => {
     await fs.access(folderPath, fs.constants.R_OK);
   } catch (err) {
     console.log(`create folder for content ${folderPath}`)
-    await fs.mkdir(folderPath);
+    await fs.mkdir(folderPath, { recursive: true });
     console.log('mark!! finish22');
   }
 };
