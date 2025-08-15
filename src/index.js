058608308a4e860363934b63e1827b85edef1d2e
Alex <alexamuek@gmail.com>
2025-08-15 22:36:05 +0800

    add logs

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const loadHTML = async (url, outputPath = defaultPath) => {
     await isExistedFolder(outputPath)
   }
   const html = await getHTML(url)
+  console.log(`url in loadHTML: ${url}`)
   const URLwithoutProtocol = url.substring(url.indexOf('//') + 2)
   const fileName = `${_.replace(URLwithoutProtocol, /[^0-9a-zA-Z]/g, '-')}.html`
   const contentFolderName = `${_.replace(URLwithoutProtocol, /[^0-9a-zA-Z]/g, '-')}_files`
